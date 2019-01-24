a4bdd395237c5d9224db64c1c75ad51730e191b8
Milan Bhatiya <milanbhatiya3@gmail.com>
2019-01-24 11:44:12 +0530

    add logincontreller and registercontroller python files add registerdao and registervo python files

--- project/__init__.py
+++ project/__init__.py
@@ -2,10 +2,20 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+import project.com.controller
+
 @app.route('/')
 def home():
-    return render_template('admin/sign-up.html')
+    return render_template('admin/index.html')
 
 @app.route('/viewcomplain')
-def managecomplain():
-    return render_template('admin/managecomplain.html')
+def viewcomplain():
+    return render_template('admin/viewcomplain.html')
+
+@app.route('/viewcomplain')
+def viewcomplain():
+    return render_template('admin/viewcomplain.html')
+
+@app.route('/viewcomplain')
+def viewcomplain():
+    return render_template('admin/viewcomplain.html')
